ad2abdc429e35758c1afdd89b189275d97b76b7a
Hristo Tanev <hristotanev7@gmail.com>
2016-07-30 17:08:51 +0100

    '

---
+++ src/js/client.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ChatApp from './pages/ChatApp';
+
+const app=document.getElementById('app');
+ReactDOM.render(<ChatApp />, app);
